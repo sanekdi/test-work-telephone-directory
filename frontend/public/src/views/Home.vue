f2b7acd9fc710fa06bee42268b045ef97c5eae77
sanekdi <progtime@mail.ru>
2022-02-06 22:20:58 +0500

    Change position button add

--- frontend/public/src/views/Home.vue
+++ frontend/public/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <v-button @click="isModalShow=true" class="btn-orange">Добавить запись</v-button>
+    <p class="wrap-create">
+      <v-button @click="isModalShow=true" class="btn-orange">Добавить запись</v-button>
+    </p>
     <v-modal :show="isModalShow" @close="closeModal">
       <phonebook-form @close="closeModal" />
     </v-modal>
@@ -29,6 +31,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrap-create {
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: right;
+  padding-right: 20px;
+}
 .home {
   margin-bottom: 40px;
 }
